6f0cf62a92f3a8a2883616703744b00b6c9aac1e
EllAchE <26192612+EllAchE@users.noreply.github.com>
2022-09-13 16:59:17 -0700

    fix build errors

--- src/components/CategoryInput.tsx
+++ src/components/CategoryInput.tsx
@@ -1,1 +1,14 @@
 // TODO: this should allow selection from some prechosen categories
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import { GridCell } from './GridCell';
+
+export function GridRow(props: any) {
+  return (
+    <Grid container justifyContent={'center'}>
+      {props.row.map((cellText: string) => {
+        return <GridCell item>{cellText}</GridCell>;
+      })}
+    </Grid>
+  );
+}
--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,1 +1,14 @@
 // TODO: this should be where users put their word lists (comma separated)
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import { GridCell } from './GridCell';
+
+export function GridRow(props: any) {
+  return (
+    <Grid container justifyContent={'center'}>
+      {props.row.map((cellText: string) => {
+        return <GridCell item>{cellText}</GridCell>;
+      })}
+    </Grid>
+  );
+}
