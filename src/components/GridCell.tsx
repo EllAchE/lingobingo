15027a20d19c91941baba9ef442213a2448002d1
EllAchE <26192612+EllAchE@users.noreply.github.com>
2022-09-17 16:21:47 -0700

    reduce height

--- src/components/GridCell.tsx
+++ src/components/GridCell.tsx
@@ -18,7 +18,7 @@ export default function GridCell({ position, children }: any) {
       alignItems={'center'}
       xs={2}
       border={1}
-      height={'105px'}
+      height={'100px'}
       textAlign={'center'}
       key={position}
       sx={{
