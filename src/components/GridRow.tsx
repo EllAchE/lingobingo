87632879c16f1c9a6a471b685d4e386ee4020462
EllAchE <26192612+EllAchE@users.noreply.github.com>
2022-09-20 21:23:51 -0700

    fix size of grid

--- src/components/GridRow.tsx
+++ src/components/GridRow.tsx
@@ -5,14 +5,20 @@ import GridCell from './GridCell';
 export default function GridRow(props: any) {
   const { row, rowIndex } = props;
   return (
-    <Grid className="px-2 sm:px-8" sx={{ maxWidth: '800px' }} container>
-      {row.map((cellText: string, cellIndex: number) => {
-        return (
-          <GridCell key={cellIndex} position={4 * rowIndex + cellIndex} item>
-            {cellText}
-          </GridCell>
-        );
-      })}
+    <Grid item xs={12}>
+      <Grid
+        container
+        className="px-2 sm:px-8"
+        sx={{ margin: 'auto', width: '700px' }}
+      >
+        {row.map((cellText: string, cellIndex: number) => {
+          return (
+            <GridCell key={cellIndex} position={4 * rowIndex + cellIndex} item>
+              {cellText}
+            </GridCell>
+          );
+        })}
+      </Grid>
     </Grid>
   );
 }
