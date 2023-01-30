0597cfe02015420e9a82d2e55bd7f28cee6e753b
EllAchE <26192612+EllAchE@users.noreply.github.com>
2023-01-29 18:42:36 -0800

    a

--- src/RapApp.tsx
+++ src/RapApp.tsx
@@ -1,23 +1,31 @@
-import React, { useRef } from 'react';
-import { Grid } from '@mui/material';
+import React, { useRef, useState } from 'react';
+import { Button, Grid } from '@mui/material';
 import MovingText from './components/rapApp/RapText';
 import RapMenu from './components/rapApp/RapMenu';
 
 function RapApp() {
   let animationRef = useRef(null);
+  const [a, setA] = useState(true);
+
   return (
     <>
-      <canvas className="-z-40 absolute top-0 left-0" id="cv"></canvas>
-      <Grid
-        container
-        sx={{ height: '100%' }}
-        direction="row"
-        alignItems="center"
-        justifyContent={'center'}
-      >
-        <MovingText animationRef={animationRef} />
-        <RapMenu animationRef={animationRef} />
-      </Grid>
+      {a ? (
+        <Button onClick={() => setA(false)}>Want to rhyme?</Button>
+      ) : (
+        <>
+          <canvas className="-z-40 absolute top-0 left-0" id="cv"></canvas>
+          <Grid
+            container
+            sx={{ height: '100%' }}
+            direction="row"
+            alignItems="center"
+            justifyContent={'center'}
+          >
+            <MovingText animationRef={animationRef} />
+            <RapMenu animationRef={animationRef} />
+          </Grid>
+        </>
+      )}
     </>
   );
 }
